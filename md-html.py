618413b16eecbd81ade1492601c1b9054cf9e92c
jeromepalayoor <me@jp-ch.gq>
2023-03-21 13:51:28 +0800

    'updated stuff'

---
+++ md-html.py
@@ -0,0 +1,9 @@
+from markdown import markdown
+
+text = ""
+filename = input("Filename of markdown file (without .md):")
+with open(f"{filename}.md", 'r') as f:
+    text = f.read()
+
+md = markdown(text)
+print(md)
---
+++ test.md
@@ -0,0 +1,9 @@
+## test bro
+
+# testing 2
+
+-----
+
+1. A
+2. B
+3. C
