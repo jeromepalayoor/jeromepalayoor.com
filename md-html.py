6466752cefc1c71891bbdca45a120c951fe8dcaf
jeromepalayoor <me@jp-ch.gq>
2023-04-07 15:58:59 +0800

    idk

--- md-html.py
+++ md-html.py
@@ -1,7 +1,7 @@
 from markdown import markdown
 
 text = ""
-filename = input("Filename of markdown file (without .md):")
+filename = input("Filename of markdown file (without .md): ")
 with open(f"{filename}.md", 'r') as f:
     text = f.read()
 
